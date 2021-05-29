54f81b4b560b4bff7b7e656b562ca6e1b03f2561
NoortanAbuDarwe <>
2021-05-29 10:43:55 +0000

    add ajavascript file

---
+++ app.js
@@ -0,0 +1,19 @@
+// great first java script file to yhis web 
+document.write("<p>Noortan</p>");
+var userName = "welcom to my webpage "
+alert(userName) ;
+console.log( userName);
+if( userName ==="welcom to my webpage " ){ console.log("hi")}
+
+ else{console.log("welcome")}
+
+var userName = prompt( "what is your name ?") ;
+alert( "welcome to our webpage"+userName);
+
+var gender1 = "BOY";
+var gender2 = "Girl";
+var gender = prompt("what is your gender?"); 
+
+if(gender=== gender1){document.write("welcome to kids Accessories for boys  ")}
+else if(gender=== gender2){document.write("welcome to kids Accessories girls ")}
+else{ document.write( "please enter genger as BOY or Girl just ")}
